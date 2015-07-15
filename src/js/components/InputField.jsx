0954c70a369005628999e60dd0a4220f96a7f13e
Casper Fabricius <me@casperfabricius.com>
2015-07-15 15:06:36 +0200

    Basic textarea with dummy data added to InputField component

--- src/js/components/InputField.jsx
+++ src/js/components/InputField.jsx
@@ -9,7 +9,9 @@ const InputField = React.createClass({
 	},
 
 	getInitialState() {
-    return {};
+    return {
+    	value: "Id,Company Name,Founder,City,Country,Postal Code, Street,Photo,Home Page,Garage Latitude,Garage Longitude\n1,Google,Larry Page & Sergey Brin,Mountain View,USA,CA 94043,1600 Amphitheatre Pkwy,http://interviewsummary.com/wp-content/uploads/2013/07/larry-page-and-sergey-brin-of-google-620x400.jpg,http://google.com,37.457674,-122.163452\n2,Apple,Steve Jobs & Steve Wozniak,Cupertino,USA,CA 95014,1 Infinite Loop,http://i.dailymail.co.uk/i/pix/2013/02/08/article-2275512-172E13BB000005DC-732_634x505.jpg,http://apple.com,37.3403188,-122.0581469\n3,Microsoft,Bill Gates,Redmond,USA,WA 98052-7329,One Microsoft Way,http://postdefiance.com/wp-content/uploads/2013/02/bill-gates-microsoft-young.jpg,http://microsoft.com,37.472189,-122.190191"
+    };
 	},
 
 	componentDidMount() {
@@ -17,7 +19,7 @@ const InputField = React.createClass({
 
 	render() {
 		return (
-			<h1>InputField</h1>
+			<textarea defaultValue={this.state.value} />
 		);
 	}
 
